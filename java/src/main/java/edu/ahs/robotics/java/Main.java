930c7296bd08fa3dbfb264509d0346babc0afe4a
LarsEarl <67292600+LarsEarl@users.noreply.github.com>
2020-06-24 16:57:00 -0600

    finished subdivide method

--- java/src/main/java/edu/ahs/robotics/java/Main.java
+++ java/src/main/java/edu/ahs/robotics/java/Main.java
@@ -3,5 +3,7 @@ package edu.ahs.robotics.java;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World! My name is Lars!");
+
+        Point a = new Point(1,2);
     }
 }
